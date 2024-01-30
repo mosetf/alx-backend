70850251b5e0fa2ce5d9ee1c915d4404764a03a4
mosetf <stanlym315@gmail.com>
2024-01-30 23:45:37 +0300

    create docs

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -32,3 +32,12 @@ class FIFOCache(BaseCaching):
         pass
 
     def get(self, key):
+        """
+        Retrieve the value associated with the given key from the cache.
+
+        Args:
+            key: The key to retrieve the value for.
+
+        Returns:
+            The value associated with the key, or None if the key is not found in the cache.
+        """
