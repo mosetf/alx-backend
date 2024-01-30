63d379dd3772d291215c523b4439aecc67d7e56a
mosetf <stanlym315@gmail.com>
2024-01-30 23:30:31 +0300

    create docs for method get

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,4 +20,13 @@ class BasicCache(BaseCaching):
             pass
 
     def get(self, key):
+            """
+            Retrieve the value associated with the given key from the cache.
+
+            Args:
+                key: The key to retrieve the value for.
+
+            Returns:
+                The value associated with the key, or None if the key is not found in the cache.
+            """
     
