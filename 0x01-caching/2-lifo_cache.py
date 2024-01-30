3e07ae695320b4b02dd17bbddf2e38b8f594537e
mosetf <stanlym315@gmail.com>
2024-01-30 23:54:28 +0300

    create docs

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,4 +11,13 @@ class LIFOCache(BaseCaching):
     It inherits from the BaseCaching class and implements the caching
     functionality using the LIFO strategy.
     """
+
     def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: The key of the item.
+            item: The item to be added to the cache.
+        """
+        
