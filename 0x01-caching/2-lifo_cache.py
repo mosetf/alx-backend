a91db96b9a9f400d9ea15f5bf574a1181bae2903
mosetf <stanlym315@gmail.com>
2024-01-30 23:52:56 +0300

    create class LIFOCache and docs

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -6,3 +6,10 @@ from BaseCaching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """
+    LIFOCache class represents a Last-In-First-Out (LIFO) caching system.
+    It inherits from the BaseCaching class and implements the caching
+    functionality using the LIFO strategy.
+    """
+    # Rest of the code...
+    
