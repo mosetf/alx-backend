c2608974cd34fb935a4876271879d1b38268b116
mosetf <stanlym315@gmail.com>
2024-01-30 23:35:06 +0300

    fix pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,7 +7,8 @@ FIFO caching
 class FIFOCache(BaseCaching):    """
     Represents a First-In-First-Out (FIFO) cache implementation.
 
-    This cache follows the FIFO eviction policy, where the least recently used item is evicted
+    This cache follows the FIFO eviction policy, where the least
+    recently used item is evicted
     when the cache reaches its maximum capacity.
 
     Inherits from the BaseCaching class.
