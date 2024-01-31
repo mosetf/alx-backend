4c40711963854662933b172d19241aa6a9cda9b0
mosetf <stanlym315@gmail.com>
2024-02-01 00:45:08 +0300

    Refactor LFUCache put method to handle cache eviction

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -52,11 +52,28 @@ class LFUCache(BaseCaching):
         self.keys_freq.insert(ins_pos, [mru_key, mru_freq])
 
     def put(self, key, item):
-            """
-            Add or update an item in the cache.
+        """
+        Add or update an item in the cache.
 
-            Args:
-                key: The key of the item.
-                item: The item to be added or updated.
-            """
-                    
+        Args:
+            key: The key of the item.
+            item: The item to be added or updated.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lfu_key, _ = self.keys_freq[-1]
+                self.cache_data.pop(lfu_key)
+                self.keys_freq.pop()
+                print("DISCARD:", lfu_key)
+            self.cache_data[key] = item
+            ins_index = len(self.keys_freq)
+            for i, key_freq in enumerate(self.keys_freq):
+                if key_freq[1] == 0:
+                    ins_index = i
+                    break
+            self.keys_freq.insert(ins_index, [key, 0])
+        else:
+            self.cache_data[key] = item
+            self.__reorder_items(key)
