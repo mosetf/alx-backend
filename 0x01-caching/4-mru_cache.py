ec0f72112df45ede4eb18db2539f0ad512d8585d
mosetf <stanlym315@gmail.com>
2024-02-01 00:32:04 +0300

    Add MRUCache class for LIFO caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+LIFO Caching
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    LIFOCache class represents a Last-In-First-Out (LIFO) caching system.
+    It inherits from the BaseCaching class and implements the caching
+    functionality using the LIFO strategy.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the FIFOCache class.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: The key of the item.
+            item: The item to be added to the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve the value associated with the given key from the cache.
+        Args:
+            key: The key to retrieve the value for.
+
+        Returns:
+            The value associated with the key, or None
+            if the key is not found in the cache.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
