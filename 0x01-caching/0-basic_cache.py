d5d7368534d1f4a072a28437051f8c669fddc654
mosetf <stanlym315@gmail.com>
2024-01-31 14:22:24 +0300

    implement the methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,32 +2,42 @@
 """
 Basic dictionary
 """
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """
     BasicCache class that extends the BaseCaching class.
     """
+    def __init__(self):
+        """
+        initialize
+        """
+        super().__init__(self)
 
     def put(self, key, item):
-            """
-            Add an item to the cache with the specified key.
+        """
+        Add an item to the cache with the specified key.
 
-            Args:
-                key: The key to associate with the item.
-                item: The item to be added to the cache.
-            """
-            pass
+        Args:
+            key: The key to associate with the item.
+            item: The item to be added to the cache.
+        """
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        self.cache_data[key].append(item)
 
     def get(self, key):
-            """
-            Retrieve the value associated with the given key from the cache.
+        """
+        Retrieve the value associated with the given key from the cache.
 
-            Args:
-                key: The key to retrieve the value for.
+        Args:
+            key: The key to retrieve the value for.
 
-            Returns:
-                The value associated with the key, or None if the key is not found in the cache.
-            """
-            pass
+        Returns:
+            The value associated with the key, or None
+            if the key is not found in the cache.
+        """
+        if key in self.cache_data is not None:
+            return self.cache_data[item]
+        return None
