074d1e763e6ae7b0c73e124ba2a085867f2350cc
mosetf <stanlym315@gmail.com>
2024-02-06 16:51:37 +0300

    Add initial Flask app with home route

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """
+    Renders the home page.
+
+    Returns:
+        The rendered template for the home page.
+    """
+
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
